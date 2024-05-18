d9e6834e05fc7be1c8492bad768eb0bc09a86e5c
Kirill K <kknyazev1992@gmail.com>
2024-05-18 12:20:08 +0500

    const

--- js/service.js
+++ js/service.js
@@ -10,39 +10,39 @@ const swiper = new Swiper('.swiper', {
 }
 });
 
-} else  if (window.matchMedia("(max-width: 1119px)").matches){
+} else if (window.matchMedia(" (max-width: 1119px) ").matches){
 
 
-  let buttonOpen = document.querySelector('.service__button');
-  let mdHiddenList = document.querySelectorAll('.md-hidden');
-  buttonOpen.addEventListener('click', function () {
+  const buttonOpen = document.querySelector('.service__button');
+  const mdHiddenList = document.querySelectorAll('.md-hidden');
+  buttonOpen.addEventListener('click', function() {
     for(let i = 0; i < mdHiddenList.length; i++) {
-      let hiddenItem = mdHiddenList[i];
+      const hiddenItem = mdHiddenList[i];
       hiddenItem.classList.toggle('md-hidden');
     }
     buttonOpen.classList.toggle('service__button--close');
     if (buttonOpen.classList.contains('service__button--close')) {
-    buttonOpen.innerHTML="Скрыть"; 
+    buttonOpen.textContent="Скрыть"; 
     } else {
-      buttonOpen.innerHTML="Показать все";
+      buttonOpen.textContent="Показать все";
     }
         });
 
   
 } else {
 
-  let buttonOpen = document.querySelector('.service__button');
-  let lgHiddenList = document.querySelectorAll('.lg-hidden');
+  const buttonOpen = document.querySelector('.service__button');
+  const lgHiddenList = document.querySelectorAll('.lg-hidden');
   buttonOpen.addEventListener('click', function () {
     for(let i = 0; i < lgHiddenList.length; i++) {
-      let hiddenItem = lgHiddenList[i];
+      const hiddenItem = lgHiddenList[i];
       hiddenItem.classList.toggle('lg-hidden');
     }
     buttonOpen.classList.toggle('service__button--close');
     if (buttonOpen.classList.contains('service__button--close')) {
-    buttonOpen.innerHTML="Скрыть"; 
+    buttonOpen.textContent="Скрыть"; 
     } else {
-      buttonOpen.innerHTML="Показать все";
+      buttonOpen.textContent="Показать все";
     }
         });
 }
